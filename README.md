11ca71b8c45e6099b7999a62ba48e88d0d3a5ea2
Scott Mudge <mail@scottmudge.com>
2022-04-10 14:04:39 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # SegoDec
+
 Simple, reliable seven-segment OCR using OpenCV
+
+## About
+
+My primary aim was to be able to read and decode a simple seven-segment display ony my RSA token with the following assumptions about the display:
+
+* Known number of digits.
+* The image is positionally stable and has stable lighting.
+* No decimals or other exotic characters.
+
+I was having issues using the `ssocr` project being unreliable and slow. It was highly dependent on lighting and often failed unless tuned exactly right.
+
+I figured that, given the limitations specified above, the actual process of decoding/reading the image is, in practice, simple. My method is as follows:
+
+1. Crop the image such that the characters have very little excess margins.
+2. Process the image to maximize contrast of the characters against the backdrop (CLAHE + brightness contrast, though optionally erosion and other morphological filters can be applied).
+3. Cut up the image based on character width and inter-character distances. The inter-character distance can be specified per-character, as at least in the display I was decoding, some characters were farther apart than others.
+4. Establish a mean pixel value with a certain window size (e.g., 4x4 window) to reduce noise for a certain number of test points for each possible segment.
+5. Compare the pixel values against a threshold to determine which segments are on or off.
+6. Compare the arrangement of on and off segments against a table of known arrangements.
+7. Return the value of each character based on which digit is matched in the reference table.
+
+This method is much faster and much more reliable than I was ever able to achieve with `ssocer` or other approaches.
+
+## Usage
+
+I did not spend a lot of time making adding a configuration interface (CLI, etc), so any changes to the configuration need to be done in `segodec.py`.
+
+**Note:** The screen I used was black characters on white background. If you're using bright characters on a black background, enable `Invert` parameter described below.
+
+Configuration values:
+
+* `NumChars`: Number of digits to decode in screen.
+* `CropX`/`CropY`/`CropW`/`CropH`: the x,y and width, height of the crop window. See example images below for how to best do this.
+* `ThresholdPct`: The threshold value (0.0-1.0) to determine if a pixel is on or off.
+* `Invert`': Set to `True` if you are using a **bright character** on **dark background**.
+* `CharStartX`/`CharStartY`: The X,Y coordinate (from top-left) of the corner of the first character. Set to `0, 0` if your crop is very close.
+* `CharHeight`/`CharWidth`: The width and height of each digit/character (gets cropped out individually).
+* `CharSpacing`: A tuple `NumChars-1` long describing the distance between one-char and another in pixels.
+* `SegmentTestPoints`: A set of tuples of (x,y) pixel coordinates for each of the 7 segments, from which a mean pixel value is dervied.
+* `TestWindowSize`: The size of the NxN window from which a mean pixel value is determined for each of the test points described in the line above.
+
+
+**NOTE**
+
+You may need to modify the `SegmentMask` table if your segments are not configured to be active for each digit as mine is. The segments are numbered as such:
+
+        ####          0 0
+       #    #       5     1
+       #    #       5     1
+        ####   ==>    6 6
+       #    #       4     2
+       #    #       4     2
+        ####          3 3
+
+To process and image, all you need to do is run:
+
+`python3 segodec.py [img]` 
+
+Where `[img]` is the path to the input image. Any standard format used by OpenCV's `imread` should be fine... jpeg, png, bmp, etc.
---
+++ segodec.py
@@ -0,0 +1,254 @@
+import cv2
+import cv2 as cv
+import sys
+import os
+
+import numpy
+import numpy as np
+
+
+RootPath = os.path.dirname(os.path.realpath(__file__))
+Quiet = True
+
+"""
+Segment Mask - Segments are index like so:
+
+    ####          0 0
+   #    #       5     1
+   #    #       5     1
+    ####   ==>    6 6
+   #    #       4     2
+   #    #       4     2
+    ####          3 3
+
+    The segment mask indicates which segments are active
+    during each displayed number.
+"""
+SegmentMask = (
+    (1, 1, 1, 1, 1, 1, 0),  # 0
+    (0, 1, 1, 0, 0, 0, 0),  # 1
+    (1, 1, 0, 1, 1, 0, 1),  # 2
+    (1, 1, 1, 1, 0, 0, 1),  # 3
+    (0, 1, 1, 0, 0, 1, 1),  # 4
+    (1, 0, 1, 1, 0, 1, 1),  # 5
+    (1, 0, 1, 1, 1, 1, 1),  # 6
+    (1, 1, 1, 0, 0, 1, 0),  # 7
+    (1, 1, 1, 1, 1, 1, 1),  # 8
+    (1, 1, 1, 1, 0, 1, 1)   # 9
+)
+
+NumSegments = 7
+
+# Globals
+# -------------------------------------------------------------------------------
+NumChars = 6
+
+CropX = 113
+CropY = 169
+CropW = 595
+CropH = 163
+
+ThresholdPct = 0.5
+
+# Default is black characters on white background
+Invert = False
+
+CharStartX = 4
+CharStartY = 15
+CharHeight = 143
+CharWidth = 83
+
+
+# List of values describing space after each character.
+CharSpacing = (
+    12, 12, 45, 12, 12
+    )
+
+# A set of points (tuple of (x,y) tuples) for each of the 7 segments
+SegmentTestPoints = (
+    ((29, 20),  (49, 20)),       # 0
+    ((68, 30),  (68, 56)),       # 1
+    ((68, 93),  (68, 113)),      # 2
+    ((29, 130), (49, 130)),      # 3
+    ((13, 93),  (13, 113)),      # 4
+    ((13, 30),  (13, 56)),       # 5
+    ((29, 74),  (49, 74)),       # 6
+)
+
+# How many pixels around test point to average., NxN
+TestWindowSize: int = 3
+# -------------------------------------------------------------------------------
+
+
+# Utility Functions
+def apply_brightness_contrast(input_img, brightness=0, contrast=0):
+    if brightness != 0:
+        if brightness > 0:
+            shadow = brightness
+            highlight = 255
+        else:
+            shadow = 0
+            highlight = 255 + brightness
+        alpha_b = (highlight - shadow) / 255
+        gamma_b = shadow
+
+        buf = cv.addWeighted(input_img, alpha_b, input_img, 0, gamma_b)
+    else:
+        buf = input_img.copy()
+
+    if contrast != 0:
+        f = 131 * (contrast + 127) / (127 * (131 - contrast))
+        alpha_c = f
+        gamma_c = 127 * (1 - f)
+
+        buf = cv.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+
+    return buf
+
+
+def print_usage():
+    print("Usage: segodec.py [input] [options]")
+    print("\tOptions:")
+    print("\t: #Todo")
+
+
+# CORE FUNCTIONS
+# -------------------------------------------------------------------------------
+def load_image(filename: str) -> np.ndarray:
+    """Loads and crops image."""
+    if not os.path.exists(filename):
+        raise FileNotFoundError("Input file not found.")
+    in_img = cv.imread(filename, cv2.IMREAD_GRAYSCALE)
+    return in_img[CropY:CropY + CropH, CropX:CropX + CropW]
+
+
+def proc_image(inp: np.ndarray) -> np.ndarray:
+    clip = 4.1
+    grid = (5, 3)
+    #kernel = np.ones((8, 8), np.uint8)
+    clahe = cv.createCLAHE(clipLimit=clip, tileGridSize=grid)
+
+    # inp = cv.fastNlMeansDenoising(inp, None, 5, 7, 21)
+    # inp = cv2.morphologyEx(inp, cv.MORPH_OPEN, kernel)
+    inp = clahe.apply(inp)
+    # morph = cv2.morphologyEx(morph, cv.MORPH_CLOSE, kernel)
+    return apply_brightness_contrast(clahe.apply(inp), 50, 100)
+
+
+def determine_segment(img: np.ndarray) -> int:
+    if len(img.shape) > 2 and img.shape[2] != 1:
+        raise TypeError("Input image is not grayscale.")
+
+    half_window = int(TestWindowSize / 2)
+    max_val = np.iinfo(img.dtype).max
+    is_seg_active = list()
+
+    for seg in range(NumSegments):
+        test_pts = SegmentTestPoints[seg]
+        num_pts = len(test_pts)
+        pt_vals = list()
+
+        for pt_idx in range(num_pts):
+            num_px = 0
+            px_val: int = 0
+            pt = test_pts[pt_idx]
+            for x in range(-half_window, half_window):
+                for y in range(-half_window, half_window):
+                    px_val += int(img.item(pt[1] + y, pt[0] + x))
+                    num_px += 1
+            pt_vals.append(int(px_val / num_px))
+
+        seg_mean = sum(pt_vals) / num_pts
+        pct = float(seg_mean / max_val)
+        active = 0
+
+        if not Invert:
+            if pct <= ThresholdPct:
+                active = 1
+        else:
+            if pct >= ThresholdPct:
+                active = 1
+
+        is_seg_active.append(active)
+
+    for seg_num, mask in enumerate(SegmentMask):
+        if len(is_seg_active) != len(mask):
+            raise ValueError("seg_active size != mask size")
+
+        matched = True
+
+        for x, seg_val in enumerate(mask):
+            if seg_val != is_seg_active[x]:
+                matched = False
+                break
+
+        if matched:
+            return seg_num
+    return -1
+
+
+def extract_chars(img: numpy.ndarray) -> list:
+    """Crops and extracts characters into new list"""
+    out = list()
+    height, width = img.shape
+    if not Quiet:
+        print("Input Stats:\n\t> Width: %d\n\t> Height: %d\n\t> Num Chars: %d"%(width, height, NumChars))
+    cur_x = CharStartX
+    cur_y = CharStartY
+    if not Quiet:
+        print("Processing image...")
+    proc = proc_image(img)
+
+    if len(CharSpacing) < (NumChars -1):
+        raise IndexError("Not enough values in CharSpacing")
+
+    for i in range(NumChars):
+        crop_wid = CharWidth
+
+        if (cur_x + crop_wid) > width:
+            crop_wid = (width - cur_x) - 1
+
+        out.append(proc[cur_y:(cur_y + CharHeight), cur_x:(cur_x + crop_wid)])
+
+        if i < (NumChars - 1):
+            cur_x += (CharWidth + CharSpacing[i])
+
+    return out
+
+
+if __name__ == '__main__':
+    if not Quiet:
+        print("SegoDec - Seven Segment OCR Decoder")
+        print("\t> Written By Scott Mudge")
+    out_file = str()
+
+    if len(sys.argv) < 1:
+        print_usage()
+
+    img = load_image(sys.argv[1])
+    chars = extract_chars(img)
+    digits = list()
+
+    for char in chars:
+        digits.append(determine_segment(char))
+
+    str_out = str()
+
+    for digit in digits:
+        if digit < 0 or digit > 9:
+            print("Indeterminate")
+            sys.exit(-1)
+        str_out += "%d"%digit
+
+    print(str_out)
+
+
+
+
+
+
+
+
+
+
+
